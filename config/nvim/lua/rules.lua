478ff8f70da437365372bd045e74981289dae7fc
SkettiSouls <skettisouls@gmail.com>
2024-05-04 22:08:43 -0500

    Fixed tab spacing

--- config/nvim/lua/rules.lua
+++ config/nvim/lua/rules.lua
@@ -5,9 +5,9 @@ vim.wo.relativenumber = true
 -- Set default tab spacing to 2.
 vim.go.autoindent = true
 vim.go.autoread = true
-vim.go.smarttab = 2
+vim.go.smarttab = true
 vim.go.shiftwidth = 2
-vim.go.tabstop = 2
+vim.go.softtabstop = -1
 
--- Let neovim access clipboard.
+-- Set neovim to send all copied text to the plus buffer. (i.e. delete or yank)
 vim.go.clipboard = "unnamedplus"
