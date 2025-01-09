82c22e525623039fed9c83a779fd46e3792f3ffd
SkettiSouls <skettisouls@gmail.com>
2025-01-09 14:51:18 -0600

    Split up lua configs
    Split `config/lua/config` into `base` (neovim conf) and `plugins` (plugin conf).
    Removed `suda.lua` in favor of just putting the 1 line in `rules.lua`

--- config/lua/base/rules.lua
+++ config/lua/base/rules.lua
@@ -2,7 +2,7 @@
 vim.wo.number = true
 vim.wo.relativenumber = true
 
--- Autos
+-- Automatically read buffers on change
 vim.go.autoread = true
 
 -- Set neovim to send all copied text to the plus buffer. (i.e. delete or yank)
@@ -13,3 +13,6 @@ vim.opt.foldmethod = "marker"
 
 -- Always vsplit to the right
 vim.opt.splitright = true
+
+-- Prompt for sudo password in read-only files
+vim.g.suda_smart_edit = 1
