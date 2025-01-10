bb4b5eba54cbfba51c375494b1102e948e4c609b
SkettiSouls <skettisouls@gmail.com>
2025-01-09 20:10:49 -0600

    Fix lazygit windows not closing properly
    Updated `get_win_table()` function to fix lazygit windows not closing if they were on window 1

--- config/lua/lib/lib.lua
+++ config/lua/lib/lib.lua
@@ -49,8 +49,9 @@ flake.lib.get_buf_table = get_buf_table-- }}}
 
 local function get_win_table()-- {{{
   local win_table = {}
-  for _, win in pairs(vim.api.nvim_list_wins()) do
-    local win_buf = vim.api.nvim_win_get_buf(win)
+  -- Nvim api uses window id's, but `close` (and presumably other vimscript cmds) use window *number* (the table keys)
+  for win, id in pairs(vim.api.nvim_list_wins()) do
+    local win_buf = vim.api.nvim_win_get_buf(id)
     local win_buf_name = vim.api.nvim_buf_get_name(win_buf)
     win_table[win_buf_name] = win
   end
