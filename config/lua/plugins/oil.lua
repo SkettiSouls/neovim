88e5c62f83e6e1c11b6c8d0ed9c24f4f42e29e04
SkettiSouls <skettisouls@gmail.com>
2025-01-26 00:10:00 -0600

    Rework pushd and cd for oil
    Made pushd and popd separate functions.
    Added an optional argument to pushd for skipping `vim.ui.input()`.
    Rewrote the `cd` bind to use pushd and popd.
    Switched to using `vim.notify` for error handling.
    Switched to using `vim.fn.isdirectory` instead of `pcall`.

--- config/lua/plugins/oil.lua
+++ config/lua/plugins/oil.lua
@@ -1,10 +1,69 @@
 local oil = require('oil')
 local actions = require('oil.actions')
 local telescope = require('telescope.builtin')
-local origin = "" -- Needed for `cd` keybind
 local dir_stack = {}
 
 bind('n', '-', "<cmd>Oil<cr>")
+
+local function pushd(dir)-- {{{
+  -- Allow using pushd without the prompt by passing `dir as a parameter `(for `cd` bind)
+  if dir == nil then
+    vim.ui.input({ prompt = 'Enter directory to add to stack: ' }, function(input)
+      dir = input
+      vim.cmd('echon " "') -- Clear cmd line (prevents errors in the same line)
+    end)
+  end
+
+  if dir_stack[1] == nil then
+    dir_stack[1] = oil.get_current_dir(0)
+  end
+
+  -- Show current stack when nothing is passed to the prompt
+  if dir == "" or dir == nil then
+    vim.print(dir_stack)
+    return
+  end
+
+  -- Handle relative paths and special characters
+  dir = vim.fn.expand(dir) -- Expand special paths (e.g `~` or $HOME)
+  if string.match(dir, '^/') == nil then
+    dir = oil.get_current_dir(0) .. dir
+  end
+
+  if vim.fn.isdirectory(dir) == 1 then
+    table.insert(dir_stack, dir)
+    vim.cmd.lcd(dir)
+    oil.open(dir)
+  else
+    vim.notify('Error: Directory \'' .. dir .. '\' not found.', vim.log.levels.ERROR)
+  end
+end-- }}}
+
+local function popd()-- {{{
+  local len = vim.tbl_count(dir_stack)
+  if len <= 1 then
+    vim.notify('Error: Directory stack empty.', vim.log.levels.ERROR)
+    return
+  end
+
+  vim.cmd.lcd(dir_stack[len - 1])
+  actions.open_cwd.callback()
+  table.remove(dir_stack, len)
+
+  -- Remove last directory from the stack.
+  if vim.tbl_count(dir_stack) == 1 then
+    dir_stack[1] = nil
+  end
+end-- }}}
+
+local function toggle_home()-- {{{
+  -- Jump to $HOME on first use, and then jump back on second. (Does nothing if first use is already in $HOME)
+  if vim.tbl_count(dir_stack) == 2 then
+    popd()
+  elseif oil.get_current_dir() ~= vim.env.HOME .. '/' then
+    pushd('$HOME')
+  end
+end-- }}}
 
 oil.setup({
   default_file_explorer = true,
@@ -20,66 +79,9 @@ oil.setup({
   keymaps = {
     ["q"] = "actions.close",
     ["<C-p>"] = { "actions.preview", opts = { vertical = true, split = "botright" }, },
-
-    -- pushd
-    ["gd"] = function()
-      if dir_stack[1] == nil then
-        dir_stack[1] = oil.get_current_dir(0)
-      end
-
-      vim.ui.input({ prompt = 'Enter directory to add to stack: ' }, function(input)
-        -- Show current stack when nothing is passed to the prompt
-        if input == "" or input == nil then
-          vim.print(dir_stack)
-          return
-        end
-
-        -- Handle relative paths and special characters
-        input = vim.fn.expand(input) -- Expand special paths (e.g `~` or $HOME)
-        if string.match(input, '^/') == nil then
-          input = oil.get_current_dir(0) .. input
-        end
-
-        local path_exists, error = pcall(vim.cmd.tcd, input)
-        if path_exists then
-          table.insert(dir_stack, input)
-          oil.open(input)
-        else
-          vim.api.nvim_err_writeln('\n' .. error)
-        end
-      end)
-    end,
-
-    -- popd
-    ["gD"] = function()
-      local len = vim.tbl_count(dir_stack)
-      if len <= 1 then
-        vim.api.nvim_err_writeln('Error: Directory stack empty')
-        return
-      end
-
-      vim.cmd.tcd(dir_stack[len - 1])
-      actions.open_cwd.callback()
-      table.remove(dir_stack, len)
-
-      -- Remove last directory from the stack.
-      if vim.tbl_count(dir_stack) == 1 then
-        dir_stack[1] = nil
-      end
-    end,
-
-    -- Jump to $HOME on first use, and then jump back on second.
-    ["cd"] = function()
-      if origin == "" then
-        origin = oil.get_current_dir()
-        vim.cmd.cd()
-        actions.open_cwd.callback()
-      elseif origin ~= oil.get_current_dir() then
-        vim.cmd.cd(origin)
-        actions.open_cwd.callback()
-        origin = ""
-      end
-    end,
+    ["gd"] = { pushd, desc = "Push directory to stack and jump" },
+    ["gD"] = { popd, desc = "Pop current directory off stack and jump backwards" },
+    ["cd"] = { toggle_home, desc = "Jump back and forth between $HOME and $PWD" },
 
     -- Telescope
     ["ff"] = function()
