f610cbe00f242868cea7e47f5020b5d94597d14a
SkettiSouls <skettisouls@gmail.com>
2025-03-08 01:02:10 -0600

    Fix haskell indenting

--- config/lua/base/indent.lua
+++ config/lua/base/indent.lua
@@ -5,6 +5,6 @@ vim.go.shiftwidth = 4
 
 -- 2 Spaces
 vim.api.nvim_create_autocmd("FileType", {
-  pattern = {"lua", "nix", "hs", "nu", "sh"},
+  pattern = {"lua", "nix", "haskell", "nu", "sh"},
   command = "setlocal shiftwidth=2 tabstop=2 expandtab"
 })
