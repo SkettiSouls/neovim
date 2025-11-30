4c63032a64c8911b0ef3072765f53dd752f95f53
SkettiSouls <skettisouls@gmail.com>
2025-11-30 14:31:04 -0600

    Fix go file indentation rules

--- config/indent.lua
+++ config/indent.lua
@@ -1,10 +1,22 @@
--- Default
+-- Default (overridden by filetype plugins)
 vim.go.autoindent = true
 vim.go.tabstop = 4
 vim.go.shiftwidth = 4
+vim.go.expandtab = false
 
--- 2 Spaces
+local two_spaced = {
+  "lua",
+  "nix",
+  "haskell",
+  "sh"
+}
+
 vim.api.nvim_create_autocmd("FileType", {
-  pattern = {"lua", "nix", "haskell", "nu", "sh"},
+  pattern = two_spaced,
   command = "setlocal shiftwidth=2 tabstop=2 expandtab"
+})
+
+vim.api.nvim_create_autocmd("FileType", {
+  pattern = { "go" },
+  command = "setlocal shiftwidth=4 tabstop=4"
 })
