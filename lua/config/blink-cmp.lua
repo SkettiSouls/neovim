a3f7022513e08515afb1e2d4ad779bb840dda4ef
SkettiSouls <skettisouls@gmail.com>
2026-03-17 20:35:39 -0500

    Make blink.cmp windows prettier

--- lua/config/blink-cmp.lua
+++ lua/config/blink-cmp.lua
@@ -1,15 +1,32 @@
 local blink = require('blink.cmp')
 
 blink.setup({
+  appearance = { nerd_font_variant = 'mono' },
+  signature = { enabled = true },
+
   completion = {
-    documentation = { auto_show = true },
-    ghost_text = { enabled = true }
+    ghost_text = { enabled = true },
+    documentation = {
+      auto_show = true,
+      auto_show_delay_ms = 250,
+      window = {
+        border = 'rounded',
+        -- TODO: Figure out how to set the highlighting of the separator
+        winhighlight = 'Normal:BlinkCmpDoc,FloatBorder:None,EndOfBuffer:BlinkCmpDoc',
+      },
+      -- TODO?: Customize how the doc window is drawn
+      -- draw = function(data)
+      -- end
+    },
+    menu = {
+      border = 'rounded',
+      draw = { align_to = 'none', },
+      winhighlight = 'Normal:Normal,FloatBorder:None,CursorLine:BlinkCmpMenuSelection,Search:None'
+    },
   },
 
   keymap = {
     preset = 'super-tab',
-    ['<C-K>'] = { 'select_prev', 'fallback' },
-    ['<C-J>'] = { 'select_next', 'fallback' },
     ['<C-S-K>'] = { function(cmp) cmp.scroll_documentation_up(1) end },
     ['<C-S-J>'] = { function(cmp) cmp.scroll_documentation_down(1) end },
   },
